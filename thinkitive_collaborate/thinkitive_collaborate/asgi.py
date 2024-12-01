648a93c4f0329ab7c43a38a96b823b2e277dd9f6
shubhamUmbarkar26 <shubham.umbarkar@baysquare.com>
2024-12-01 17:16:44 +0530

    redirect to home url made changes in views.py

--- thinkitive_collaborate/thinkitive_collaborate/asgi.py
+++ thinkitive_collaborate/thinkitive_collaborate/asgi.py
@@ -2,9 +2,9 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from thinkitive_app.routing import websocket_urlpatterns  # Your app's routing file
+from thinkitive_app.routing import websocket_urlpatterns 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'collaborative_editor.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thinkitive_collaborate.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
